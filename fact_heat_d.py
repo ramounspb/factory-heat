fb2bf8cf59fb40c6b3da69e78950986725c13683
ramounspb <ramounspb@yahoo.com>
2021-11-12 16:15:58 +0300

    update bilds and fact heat

--- fact_heat_d.py
+++ fact_heat_d.py
@@ -7,34 +7,93 @@ conn = sqlite3.connect('factoryheatdb.sqlite')
 cur = conn.cursor()
 
 cur.executescript('''
-CREATE TABLE IF NOT EXISTS Fact_Heat_d(
+DROP TABLE IF EXISTS Fact_Heat_d;
+CREATE TABLE Fact_Heat_d(
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     bildings_id INTEGER NOT NULL,
+    date DATE,
     fact_heat_all INTEGER,
-    fact_heat_ch INTEGER,
     mass_in INTEGER,
     mass_out INTEGER,
+    mass_delta INTEGER,
     temp_in INTEGER,
     temp_out INTEGER,
+    temp_delta INTEGER,
     press_in INTEGER,
     press_out INTEGER,
     sensor_work_time INTEGER,
     sensor_off_time INTEGER
+    fact_heat_ch INTEGER
 );
 ''')
 
+# cur.executescript('''
+# CREATE TABLE IF NOT EXISTS Fact_Heat_d(
+#     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+#     bildings_id INTEGER NOT NULL,
+#     date DATE,
+#     fact_heat_all INTEGER,
 
-# cur.execute("SELECT * FROM Bildings")
+#     mass_in INTEGER,
+#     mass_out INTEGER,
+#     mass_delta INTEGER,
+#     temp_in INTEGER,
+#     temp_out INTEGER,
+    # temp_delta INTEGER,
+#     press_in INTEGER,
+#     press_out INTEGER,
+#     sensor_work_time INTEGER,
+#     sensor_off_time INTEGER
+#     fact_heat_ch INTEGER
+# );
+# ''')
+
+def read_sheet(bildNum, shName):
+    if bildNum == 3:
+        df1 = pd.read_excel("./other/Посуточная ведомость1.xlsx",sheet_name='КМ-5 ИТП1 ЗУпр')
+        df1 = df1.drop(df1.columns[[0]], axis=1)
+        df1 = df1.drop(range(0,9))
+        df1 = df1.drop(range(40,50))
+        df1.columns=['date',"fact_heat_all",'mass_in','mass_out',
+        'mass_delta','temp_in','temp_out','temp_delta','press_in',
+        'press_out','sensor_work_time','sensor_off_time']
+
+        df2= pd.read_excel("./other/Посуточная ведомость1.xlsx",sheet_name='КМ-5 ИТП2 ЗУпр')
+        df2 = df2.drop(df2.columns[[0]], axis=1)
+        df2 = df2.drop(range(0,9))
+        df2 = df2.drop(range(40,50))
+        df2.columns=['date',"fact_heat_all",'mass_in','mass_out',
+        'mass_delta','temp_in','temp_out','temp_delta','press_in',
+        'press_out','sensor_work_time','sensor_off_time']
+
+        df3 = pd.concat([df1, df2]).groupby(['date']).sum()
+        df3.iloc[:,4:]*=0.5
+        df3['bildings_id'] = bildNum
+        print(df3)
+        df3.to_sql('Fact_Heat_d', con = conn, if_exists='append', index=False)
+        #дата не грузится в БД, проверить старый код ниже на ошибки
+
+
+    # df = pd.read_excel("./other/Посуточная ведомость1.xlsx",sheet_name=shName)
+    # df = df.drop(df.columns[[0]], axis=1)
+    # df = df.drop(range(0,9))
+    # df = df.drop(range(40,50))
+    # df.columns=['date',"fact_heat_all",'mass_in','mass_out',
+    # 'mass_delta','temp_in','temp_out','temp_delta','press_in',
+    # 'press_out','sensor_work_time','sensor_off_time']
+    # df['bildings_id'] = bildNum
+    # df.to_sql('Fact_Heat_d', con = conn, if_exists='append', index=False)
+    # print(df)
+
+read_sheet(3, 'КМ-5 24 цех')
+
+# cur.execute("SELECT id,name FROM Bildings")
 # bilds = cur.fetchall()
-# for day, temp_out in final_data.items():
-#     for bild in bilds:
-#         bildings_id = bild[0]
-#         type = bild[2]
-#         load_h = bild[10]
-#         calc_heat_d = round(calc_heat(type, load_h, temp_out),5)
-#         cur.execute('''INSERT OR IGNORE INTO Calc_Heat_d
-#             (bildings_id, date, temp_out, calc_heat_d)
-#             VALUES (?, ?, ?, ?)''',
-#             (bildings_id, day, temp_out, calc_heat_d) )
+# print(bilds)
+# for bild in bilds:
+#     bildNum = bild[0]
+#     name = bild[1]
+#     shName = "КМ-5 " + name
+
 # conn.commit()
 # conn.close()
