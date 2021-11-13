7d355e3ab5ca1d57766108675e39b01e2362c61a
ramounspb <ramounspb@yahoo.com>
2021-11-13 20:31:28 +0300

    fix fact heat

--- fact_heat_d.py
+++ fact_heat_d.py
@@ -49,51 +49,42 @@ CREATE TABLE Fact_Heat_d(
 # ''')
 
 def read_sheet(bildNum, shName):
+    def drop_extra(df):
+        df = df.drop(df.columns[[0]], axis=1)
+        df = df.drop(range(0,9))
+        df = df.drop(range(40,50))
+        df.columns=['date',"fact_heat_all",'mass_in','mass_out',
+        'mass_delta','temp_in','temp_out','temp_delta','press_in',
+        'press_out','sensor_work_time','sensor_off_time']
+        return df
     if bildNum == 3:
         df1 = pd.read_excel("./other/Посуточная ведомость1.xlsx",sheet_name='КМ-5 ИТП1 ЗУпр')
-        df1 = df1.drop(df1.columns[[0]], axis=1)
-        df1 = df1.drop(range(0,9))
-        df1 = df1.drop(range(40,50))
-        df1.columns=['date',"fact_heat_all",'mass_in','mass_out',
-        'mass_delta','temp_in','temp_out','temp_delta','press_in',
-        'press_out','sensor_work_time','sensor_off_time']
-
-        df2= pd.read_excel("./other/Посуточная ведомость1.xlsx",sheet_name='КМ-5 ИТП2 ЗУпр')
-        df2 = df2.drop(df2.columns[[0]], axis=1)
-        df2 = df2.drop(range(0,9))
-        df2 = df2.drop(range(40,50))
-        df2.columns=['date',"fact_heat_all",'mass_in','mass_out',
-        'mass_delta','temp_in','temp_out','temp_delta','press_in',
-        'press_out','sensor_work_time','sensor_off_time']
-
-        df3 = pd.concat([df1, df2]).groupby(['date']).sum()
+        df1 = drop_extra(df1)
+        df2 = pd.read_excel("./other/Посуточная ведомость1.xlsx",sheet_name='КМ-5 ИТП2 ЗУпр')
+        df2 = drop_extra(df2)
+        df3 = pd.concat([df1, df2]).groupby(['date'],as_index=False).sum()
         df3.iloc[:,4:]*=0.5
         df3['bildings_id'] = bildNum
+        print(df3.info())
         print(df3)
         df3.to_sql('Fact_Heat_d', con = conn, if_exists='append', index=False)
-        #дата не грузится в БД, проверить старый код ниже на ошибки
+    else:
+        df = pd.read_excel("./other/Посуточная ведомость1.xlsx",sheet_name=shName)
+        df = drop_extra(df)
+        df['bildings_id'] = bildNum
+        df.to_sql('Fact_Heat_d', con = conn, if_exists='append', index=False)
+        print(df)
 
+cur.execute("SELECT id,name FROM Bildings")
+bilds = cur.fetchall()
+print(bilds)
+for bild in bilds:
+    bildNum = bild[0]
+    name = bild[1]
+    shName = "КМ-5 " + name
+    read_sheet(bildNum, shName)
 
-    # df = pd.read_excel("./other/Посуточная ведомость1.xlsx",sheet_name=shName)
-    # df = df.drop(df.columns[[0]], axis=1)
-    # df = df.drop(range(0,9))
-    # df = df.drop(range(40,50))
-    # df.columns=['date',"fact_heat_all",'mass_in','mass_out',
-    # 'mass_delta','temp_in','temp_out','temp_delta','press_in',
-    # 'press_out','sensor_work_time','sensor_off_time']
-    # df['bildings_id'] = bildNum
-    # df.to_sql('Fact_Heat_d', con = conn, if_exists='append', index=False)
-    # print(df)
+#добавить проверку дат
 
-read_sheet(3, 'КМ-5 24 цех')
-
-# cur.execute("SELECT id,name FROM Bildings")
-# bilds = cur.fetchall()
-# print(bilds)
-# for bild in bilds:
-#     bildNum = bild[0]
-#     name = bild[1]
-#     shName = "КМ-5 " + name
-
-# conn.commit()
-# conn.close()
+conn.commit()
+conn.close()
